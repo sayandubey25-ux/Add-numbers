b4ee031a816c50d9cdddfc6550298bd980d956b1
Sayan Dubey <sayandubey25@gmail.com>
2026-02-08 11:23:16 +0000

    a commit message

---
+++ main.py
@@ -0,0 +1,11 @@
+name=("Sayan")
+age=13
+num1=age+5
+num2=age/5
+num3=age*5
+print("Num1 is", num1,"and it is an", type(num1))
+print("Num2 is", num2,"and it is a", type(num2))
+if num1>num2:
+    print("Num1 is greater than num2!!!!!")
+elif num2>num1:
+    print("Num2 is greater than num1")
